8d146a49468468504cb6937604a3c2643d2e20f8
Kamalesh Waran <kwaranwork98@gmail.com>
2024-05-09 23:16:39 +0800

    New API Folder

---
+++ server/api/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const cors = require("cors");
+const { connect } = require("mongoose");
+require("dotenv").config();
+const upload = require("express-fileupload");
+
+const userRoutes = require("./routes/userRoutes");
+const postRoutes = require("./routes/postRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+
+const app = express();
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+
+// app.use(cors(
+//   {
+//     origin: ["https://project3-ga-frontend.vercel.app/login"],
+//     methods:["POST","GET"],
+//     credentials: true
+//   }
+// ))
+
+app.get("/", (req, res) =>
+  res.status(200).json({
+    message: "Hello World",
+  })
+);
+
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(upload());
+app.use("/uploads", express.static(__dirname + "/uploads"));
+
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server started on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
