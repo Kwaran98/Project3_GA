9e80a5205befd5db7dba5e4b354952fa8abcd080
Kwaran98 <142907620+Kwaran98@users.noreply.github.com>
2024-05-11 16:45:38 +0800

    Update PostItem.jsx

--- client/src/components/PostItem.jsx
+++ client/src/components/PostItem.jsx
@@ -23,7 +23,7 @@ const PostItem = ({
       <article className="post">
         <div className="post__thumbnail">
           <img
-            src={`${process.env.REACT_APP_ASSETS_URL}/uploads/${thumbnail}`}
+            src={`${thumbnail}`}
             alt={title}
           />
         </div>
