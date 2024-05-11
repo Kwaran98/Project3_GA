6baf2087a0d6318c2d347f1533fec380deace4dd
Kamalesh Waran <kwaranwork98@gmail.com>
2024-05-11 17:31:57 +0800

    Authors Page

--- client/src/pages/Authors.jsx
+++ client/src/pages/Authors.jsx
@@ -22,27 +22,40 @@ const Authors = () => {
     getAuthors();
   }, []);
 
-  if(isLoading){
-    return <Loader />
+  if (isLoading) {
+    return <Loader />;
   }
 
   return (
-    <section className='authors'>
-    {authors.length > 0 ? <div className='container authors_container'>
-         {authors.map(author => {
-          return <Link key={author?._id} to={`/posts/users/${author?._id}`} className='author'>
-            <div className='authors_avatar'>
-              {/* <img src={`${process.env.REACT_APP_ASSETS_URL}/uploads/${author.avatar}`} alt={`Image of ${author?.name}`} /> */}
-              <img src={`${author.avatar}`} alt={`Image of ${author?.name}`} />
-            </div>
-            <div  className='authors_info'>
-              <h4>{author?.name}</h4>
-              <p>{author?.posts}</p>
-            </div>
-          </Link>
-        })}
-    </div> : <h2 className='center'> No User/Authors found</h2>}
-  </section>
+    <section className="authors">
+      {authors.length > 0 ? (
+        <div className="container authors_container">
+          {authors.map((author) => {
+            return (
+              <Link
+                key={author?._id}
+                to={`/posts/users/${author?._id}`}
+                className="author"
+              >
+                <div className="authors_avatar">
+                  <img
+                    src={`${process.env.REACT_APP_ASSETS_URL}/uploads/${author.avatar}`}
+                    alt={`Image of ${author?.name}`}
+                  />
+                  {/* <img src={`${author.avatar}`} alt={`Image of ${author?.name}`} /> */}
+                </div>
+                <div className="authors_info">
+                  <h4>{author?.name}</h4>
+                  <p>{author?.posts}</p>
+                </div>
+              </Link>
+            );
+          })}
+        </div>
+      ) : (
+        <h2 className="center"> No User/Authors found</h2>
+      )}
+    </section>
   );
 };
 
